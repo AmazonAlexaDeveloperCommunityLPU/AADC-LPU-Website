c23fc2125c01b70083aac37f86b3587b00661f0b
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2019-03-19 19:50:19 +0530

    Basic project structure

---
+++ index.js
@@ -0,0 +1,22 @@
+express=require('express');
+path=require('path');
+bodyParser=require('body-parser');
+app=express()
+
+port = 1998;
+
+app.use(express.static(path.join(__dirname, 'public')));
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/public/index.html');
+});
+
+app.listen(port,function(){
+    console.log('Listening at port ' + port);
+});
+// Localhost Address= localhost:1998
